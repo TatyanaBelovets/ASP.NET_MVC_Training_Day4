8a428c4129329f19ea2afd2e65e34d8c16c7f16e
TatyanaBelovets <Tatyana.Belovets@gmail.com>
2015-07-20 14:53:42 +0300

    Added library and test classes

---
+++ Task1.Library/SortingForJaggedArrays.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1.Library
+{
+    public interface IJaggedArrayComparable
+    {
+        bool Compare(int[] left, int[] right, bool order);
+    }
+
+
+    public class SortingBySumOfElements : IJaggedArrayComparable
+    {
+        /// <summary>
+        /// Sort matrix by sum of elements in rows.
+        /// </summary>
+        /// <param name="left">Left operand</param>
+        /// <param name="right">Right operand</param>
+        /// <param name="orderByDescending">true - sort by descending, false - sort by ascending</param>
+        public bool Compare(int[] left, int[] right, bool orderByDescending = false)
+        {
+            return orderByDescending ? left.Sum() > right.Sum() : left.Sum() < right.Sum();
+        }
+    }
+
+    public class SortingByMaxAbsOfElements : IJaggedArrayComparable
+    {
+        /// <summary>
+        /// Sort matrix by max abs of elements in rows.
+        /// </summary>
+        /// <param name="left">Left operand</param>
+        /// <param name="right">Right operand</param>
+        /// <param name="orderByDescending">true - sort by descending, false - sort by ascending</param>
+        public bool Compare(int[] left, int[] right, bool orderByDescending = false)
+        {
+            var leftMaxModule = 0; 
+            var rightMaxModule = 0;
+            for (int i = 0; i < left.Length; i++)
+            {
+                leftMaxModule = leftMaxModule < left[i] ? left[i] : leftMaxModule;
+            }
+            for (int i = 0; i < right.Length; i++)
+            {
+                rightMaxModule = rightMaxModule < right[i] ? right[i] : rightMaxModule;
+            }
+            return orderByDescending ? leftMaxModule > rightMaxModule : leftMaxModule < rightMaxModule;
+        }
+    }
+    public class SortingForJaggedArrays
+    {
+        /// <summary>
+        /// Method for sort matrix.
+        /// </summary>
+        /// <param name="array">Matrix for sorting</param>
+        /// <param name="compare">Rule of sorting</param>
+        /// <param name="orderByDescending">true - sort by descending, false - sort by ascending</param>
+        public static void Sort(int[][] array, IJaggedArrayComparable compare, bool orderByDescending = false)
+        {
+            for (var i = 0; i < array.Length; i++)
+            {
+                var nMaxLine = i;
+                for (var j = i + 1; j < array.Length; j++)
+                {
+                    nMaxLine = compare.Compare(array[nMaxLine], array[j], orderByDescending) ? nMaxLine : j;
+                }
+                var temp = array[i];
+                array[i] = array[nMaxLine];
+                array[nMaxLine] = temp;
+            }
+        }
+    }
+}
---
+++ Task1.Tests/Task1.Tests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1.Library;
+
+namespace Task1.Tests
+{
+    [TestClass]
+    public class SortingJaggedArraysTests
+    {
+        [TestMethod]
+        public void SortingForJaggedArrays_GivenSortingByMaxAbsOfElements_ReturnSortedByMaxAbsArrayByAscending()
+        {
+            var arr = new int[5][];
+            {
+                arr[0] = new[] { 1, 3, 5, 7, 9 };
+                arr[1] = new[] { 0, 2, 4, 6 };
+                arr[2] = new[] { 11, 22 };
+                arr[3] = new[] { 11, 22, 54, 66 };
+                arr[4] = new[] { 11 };
+            }
+            var arrResult = new int[5][];
+            {
+                arrResult[0] = new[] { 0, 2, 4, 6 };
+                arrResult[1] = new[] { 1, 3, 5, 7, 9 };
+                arrResult[2] = new[] { 11 };
+                arrResult[3] = new[] { 11, 22 };
+                arrResult[4] = new[] { 11, 22, 54, 66 };
+            }
+            SortingForJaggedArrays.Sort(arr, new SortingByMaxAbsOfElements());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Assert.AreEqual(arrResult[i][j], arr[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SortingForJaggedArrays_GivenSortingByMaxAbsOfElementsAndTrue_ReturnSortedByMaxAbsArrayByDescending()
+        {
+            var arr = new int[5][];
+            {
+                arr[0] = new[] { 1, 3, 5, 7, 9 };
+                arr[1] = new[] { 0, 2, 4, 6 };
+                arr[2] = new[] { 11, 22 };
+                arr[3] = new[] { 11, 22, 54, -66 };
+                arr[4] = new[] { 11 };
+            }
+            var arrResult = new int[5][];
+            {
+                arrResult[4] = new[] { 0, 2, 4, 6 };
+                arrResult[3] = new[] { 1, 3, 5, 7, 9 };
+                arrResult[2] = new[] { 11 };
+                arrResult[1] = new[] { 11, 22 };
+                arrResult[0] = new[] { 11, 22, 54, -66 };
+            }
+            SortingForJaggedArrays.Sort(arr, new SortingByMaxAbsOfElements(), true);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Assert.AreEqual(arrResult[i][j], arr[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SortingForJaggedArrays_GivenSortingBySumOfElements_ReturnSortedByMaxAbsArrayByAscending()
+        {
+            var arr = new int[5][];
+            {
+                arr[0] = new[] { 1, 3, 5, 7, 9 };
+                arr[1] = new[] { 0, 2, 4, 6 };
+                arr[2] = new[] { 11, 22 };
+                arr[3] = new[] { 11, 22, 54, 66 };
+                arr[4] = new[] { 11 };
+            }
+            var arrResult = new int[5][];
+            {
+                arrResult[0] = new[] { 11 };
+                arrResult[1] = new[] { 0, 2, 4, 6 };
+                arrResult[2] = new[] { 1, 3, 5, 7, 9 };
+                arrResult[3] = new[] { 11, 22 };
+                arrResult[4] = new[] { 11, 22, 54, 66 };
+            }
+            SortingForJaggedArrays.Sort(arr, new SortingBySumOfElements());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Assert.AreEqual(arrResult[i][j], arr[i][j]);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void SortingForJaggedArrays_GivenSortingBySumOfElementsAndTrue_ReturnSortedByMaxAbsArrayByDescending()
+        {
+            var arr = new int[5][];
+            {
+                arr[0] = new[] { 1, 3, 5, 7, 9 };
+                arr[1] = new[] { 0, 2, 4, 6 };
+                arr[2] = new[] { 11, 22 };
+                arr[3] = new[] { 11, 22, 54, 66 };
+                arr[4] = new[] { 11 };
+            }
+            var arrResult = new int[5][];
+            {
+                arrResult[4] = new[] { 11 };
+                arrResult[3] = new[] { 0, 2, 4, 6 };
+                arrResult[2] = new[] { 1, 3, 5, 7, 9 };
+                arrResult[1] = new[] { 11, 22 };
+                arrResult[0] = new[] { 11, 22, 54, 66 };
+            }
+            SortingForJaggedArrays.Sort(arr, new SortingBySumOfElements(), true);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Assert.AreEqual(arrResult[i][j], arr[i][j]);
+                }
+            }
+        }
+    }
+}
